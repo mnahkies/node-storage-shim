74ed7246226079e41cfbc3b1d8cb30a0c8f2f487
Michael Nahkies <michael@nahkies.co.nz>
2018-09-10 18:07:43 +0100

    add typescript declaration and test

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+declare module "node-storage-shim" {
+    export = Storage
+}
---
+++ test-typescript-declarations.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Testing typescript declarations..."
+
+cd ./test-typescript-declarations
+npm install &> /dev/null
+npm link ../ &> /dev/null
+npx tsc &> /dev/null
+
+if [ $(node ./test-typescript-declarations.js) != "bar" ]; then
+    echo "Something went wrong"
+    exit 1
+else
+    echo "Typescript Declaration is working"
+    exit 0
+fi
